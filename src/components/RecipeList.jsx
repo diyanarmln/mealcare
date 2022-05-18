24f031ff139ec19544452a471a9f785ac3b96f3b
diyanarmln <diyana.rmln@gmail.com>
2022-05-19 02:32:42 +0800

    added component and handler for delete recipe

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -5,14 +5,36 @@ import { Link } from 'react-router-dom';
 export default function RecipeList() {
   const [recipes, setRecipes] = useState([]);
 
-  useEffect(() => { axios.get('/api/recipes')
-    .then((response) => {
-      setRecipes(response.data); }); }, []);
+  const getRecipes = () => {
+    axios.get('/api/recipes')
+      .then((response) => {
+        setRecipes(response.data); });
+  };
+
+  useEffect(() => { getRecipes(); }, []);
+
+  const handleDelete = async (recipeId) => {
+    // e.preventDefault();
+    try {
+      const { data } = await axios.delete(`/api/recipe/${recipeId}`);
+      console.log(data);
+      if (data.success) {
+        // useEffect(() => { getRecipes(); }, []);
+        getRecipes();
+      }
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  };
 
   const recipesList = recipes.length === 0 ? <p>No Recipes Available</p> : recipes.map((recipe) => (
     <li key={recipe.id}>
       {recipe.title}
+      <br />
+      Servings:
+      {' '}
       {recipe.servings}
+      <button type="button" className="btn btn-dark" onClick={(e) => { if (window.confirm('Delete the item?')) { e.preventDefault(); handleDelete(recipe.id); } }}>x</button>
     </li>
   ));
 
