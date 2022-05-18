5c5e71ffd690a6de20529dc2cfbf3526985cf597
diyanarmln <diyana.rmln@gmail.com>
2022-05-19 02:05:25 +0800

    fixed order of routes and typo error

--- routes.mjs
+++ routes.mjs
@@ -6,13 +6,13 @@ import initRecipesController from './controllers/recipes.mjs';
 export default function routes(app) {
   const recipesController = initRecipesController(db);
 
+  app.get('/api/recipes', recipesController.index);
+  app.post('/api/recipes', recipesController.create);
+  app.put('/api/recipe/:id/edit', recipesController.update);
+  app.get('/api/recipe/:id', recipesController.show);
+  app.delete('/api/recipe/:id', recipesController.remove);
+
   app.get('/*', (request, response) => {
     response.sendFile(resolve('dist', 'main.html'));
   });
-
-  app.get('/api/recipes', recipesController.index);
-  app.get('/api/recipe/:id', recipesController.show);
-  app.post('api/recipe/:id', recipesController.create);
-  app.put('/api/recipe/:id/edit', recipesController.update);
-  app.delete('/api/recipe/:id', recipesController.remove);
 }
