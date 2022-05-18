f2ba6107d2249880432ddaa9ba267a84317398a9
diyanarmln <diyana.rmln@gmail.com>
2022-05-19 02:05:48 +0800

    added handle submit function when user submits form

--- src/components/RecipeForm.jsx
+++ src/components/RecipeForm.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -5,6 +6,9 @@ export default function RecipeForm() {
   const [title, setTitle] = useState('');
   const [servings, setServings] = useState();
   const [instructions, setInstructions] = useState('');
+  const [category, setCategory] = useState();
+  const [successMessage, setSuccessMessage] = useState('');
+
   // photo hallo
 
   const handleTitleChange = (e) => {
@@ -17,6 +21,28 @@ export default function RecipeForm() {
 
   const handleInstructionsChange = (e) => {
     setInstructions(e.target.value);
+  };
+
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post('/api/recipes', {
+        title, servings, instructions, category,
+      });
+      if (data.success) {
+        setSuccessMessage('Recipe saved success!');
+        setTitle('');
+        setServings('');
+        setInstructions('');
+        setCategory(0);
+      }
+    } catch (err) {
+      console.log(err.response.data);
+    }
   };
 
   return (
@@ -43,6 +69,19 @@ export default function RecipeForm() {
         <br />
 
         <div className="form-group">
+          <label htmlFor="category">
+            Category:
+            <select id="category" className="form-select" onChange={handleCategoryChange}>
+              <option value="1">Breakfast</option>
+              <option value="2">Lunch</option>
+              <option value="3">Dinner</option>
+            </select>
+          </label>
+        </div>
+
+        <br />
+
+        <div className="form-group">
           <label htmlFor="instructions">
             Instructions:
             <textarea id="instructions" value={instructions} type="text" className="form-control" onChange={handleInstructionsChange} />
@@ -51,11 +90,12 @@ export default function RecipeForm() {
 
         <br />
 
-        <button type="submit" className="btn btn-primary">Save</button>
+        <button type="submit" className="btn btn-primary" onClick={handleSubmit}>Save</button>
         <Link to="/recipes">
           <button type="button" className="btn btn-outline-secondary">Cancel</button>
         </Link>
       </form>
+      {successMessage}
     </div>
   );
 }
