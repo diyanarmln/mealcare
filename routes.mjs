94ac4541092f8506721aa85cdb0bcc51bd1308c4
diyanarmln <diyana.rmln@gmail.com>
2022-05-19 00:57:41 +0800

    amended route name for delete

--- routes.mjs
+++ routes.mjs
@@ -6,13 +6,13 @@ import initRecipesController from './controllers/recipes.mjs';
 export default function routes(app) {
   const recipesController = initRecipesController(db);
 
-  app.get('/', (request, response) => {
+  app.get('/*', (request, response) => {
     response.sendFile(resolve('dist', 'main.html'));
   });
 
   app.get('/api/recipes', recipesController.index);
   app.get('/api/recipe/:id', recipesController.show);
   app.post('api/recipe/:id', recipesController.create);
-  app.put('/api/recipe/:id/edit', recipesController.edit);
-  app.delete('/api/recipe/:id', recipesController.destroy);
+  app.put('/api/recipe/:id/edit', recipesController.update);
+  app.delete('/api/recipe/:id', recipesController.remove);
 }
