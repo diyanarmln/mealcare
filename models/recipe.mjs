4e53db27c7e1f9d47f319e3f13f10b168b59e075
diyanarmln <diyana.rmln@gmail.com>
2022-05-18 21:07:40 +0800

    added model for recipe

---
+++ models/recipe.mjs
@@ -0,0 +1,42 @@
+export default function initUserModel(sequelize, DataTypes) {
+  return sequelize.define('recipe', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    userId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+    },
+    title: {
+      type: DataTypes.STRING,
+    },
+    recipeInstructions: {
+      type: DataTypes.TEXT,
+    },
+    servings: {
+      type: DataTypes.INTEGER,
+    },
+    filename: {
+      type: DataTypes.STRING,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+
+  }, {
+    // The underscored option makes Sequelize reference snake_case names in the DB.
+    underscored: true,
+  });
+}
