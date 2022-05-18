2ee8a8b1df8bd97e43313aae16a3638f742e859e
diyanarmln <diyana.rmln@gmail.com>
2022-05-18 21:17:38 +0800

    added model for plan

---
+++ models/plan.mjs
@@ -0,0 +1,42 @@
+export default function initPlanModel(sequelize, DataTypes) {
+  return sequelize.define('plan', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    breakfastRecipe: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'recipes',
+        key: 'id',
+      },
+    },
+    lunchRecipe: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'recipes',
+        key: 'id',
+      },
+    },
+    dinnerRecipe: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'recipes',
+        key: 'id',
+      },
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  }, {
+    // The underscored option makes Sequelize reference snake_case names in the DB.
+    underscored: true,
+  });
+}
