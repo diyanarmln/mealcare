08252f1759a9d39c121539789e9ef9eb89bca151
diyanarmln <diyana.rmln@gmail.com>
2022-05-18 22:47:05 +0800

    added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard.jsx';
+import RecipeList from './components/RecipeList.jsx';
+import RecipeForm from './components/RecipeForm.jsx';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div>This is App.jsx</div>
-      <Dashboard />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/recipes" element={<RecipeList />} />
+        <Route path="/new-recipe" element={<RecipeForm />} />
+        <Route path="/recipe" element={<RecipeForm />} />
+      </Routes>
     </BrowserRouter>
   );
 }
