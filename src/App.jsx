839bc59a719990422ed1f7a25ca505b8be4f51dd
diyanarmln <diyana.rmln@gmail.com>
2022-05-19 03:31:28 +0800

    updated view route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard.jsx';
 import RecipeList from './components/RecipeList.jsx';
 import RecipeForm from './components/RecipeForm.jsx';
+import RecipeView from './components/RecipeView.jsx';
 
 export default function App() {
   return (
@@ -11,7 +12,8 @@ export default function App() {
         <Route path="/" element={<Dashboard />} />
         <Route path="/recipes" element={<RecipeList />} />
         <Route path="/new-recipe" element={<RecipeForm />} />
-        <Route path="/recipe" element={<RecipeForm />} />
+        <Route path="/recipe" element={<RecipeView />} />
+        <Route path="/recipe/edit" element={<RecipeForm />} />
       </Routes>
     </BrowserRouter>
   );
