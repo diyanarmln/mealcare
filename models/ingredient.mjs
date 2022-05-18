3dd62fd368180a9cd8716674f52f3291525be306
diyanarmln <diyana.rmln@gmail.com>
2022-05-18 21:15:47 +0800

    added model for ingredient

---
+++ models/ingredient.mjs
@@ -0,0 +1,27 @@
+export default function initIngredientModel(sequelize, DataTypes) {
+  return sequelize.define('ingredient', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    unitMeasurement: {
+      type: DataTypes.STRING,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  }, {
+    // The underscored option makes Sequelize reference snake_case names in the DB.
+    underscored: true,
+  });
+}
