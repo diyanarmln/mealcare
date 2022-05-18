3d54e82dd201fe1830114749bdc74dfcc35ef190
diyanarmln <diyana.rmln@gmail.com>
2022-05-18 20:42:51 +0800

    added migration to create category_recipes table

---
+++ migrations/20220518124057-create-category-recipes-table.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('category_recipes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      category_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+      },
+      recipe_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'recipes',
+          key: 'id',
+        },
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('category_recipes');
+  }
+};
