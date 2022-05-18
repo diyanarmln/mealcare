1b35ba46de5b58d269fb811d5fc06bf04eb3453a
diyanarmln <diyana.rmln@gmail.com>
2022-05-19 00:57:21 +0800

    added controller functions for recipes

--- controllers/recipes.mjs
+++ controllers/recipes.mjs
@@ -1,19 +1,104 @@
 export default function initRecipesController(db) {
-  const index = (request, response) => {
-    db.Recipe.findAll({
+  const index = async (request, response) => {
+    try {
+      const recipes = await db.Recipe.findAll({
+        where: {
+          userId: 1,
+        },
+      });
+      const recipeArr = recipes.map((el) => ({
+        id: el.id,
+        title: el.title,
+        servings: el.servings,
+      }));
+      response.send(recipeArr);
+    } catch (err) {
+      response.status(500).send(err);
+    }
+  };
+
+  const show = async (request, response) => {
+    await db.Recipe.findOne({
       where: {
-        userId: 1,
+        id: request.params.id,
       },
     })
-      .then((recipes) => {
-        response.send(recipes);
+      .then((recipe) => {
+        response.send(recipe);
       })
       .catch((error) => console.log(error));
+  };
+
+  const create = async (request, response) => {
+    try {
+      const newRecipe = {
+        userId: 1,
+        title: request.body.title,
+        recipeInstructions: request.body.instructions,
+        servings: request.body.servings,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+      // run the DB INSERT query
+      await db.Recipe.create(newRecipe);
+
+      // create entry in join table
+      // await recipe.addUser(user);
+
+      response.send(
+        { success: true },
+      );
+    } catch (error) {
+      response.status(500).send(error);
+    }
+  };
+
+  const update = async (request, response) => {
+    const { id } = request.params;
+    const recipe = await db.Recipe.findOne({ where: { id } });
+
+    const updatedRecipe = {
+      title: request.body.title,
+      recipeInstructions: request.body.instructions,
+      servings: request.body.servings,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    try {
+      // run the DB INSERT query
+      await recipe.update(updatedRecipe);
+
+      // create entry in join table
+      // await recipe.addUser(user);
+
+      response.send(
+        { success: true },
+      );
+    } catch (error) {
+      response.status(500).send(error);
+    }
+  };
+
+  const remove = async (request, response) => {
+    try {
+      const { id } = request.params;
+      const recipe = await db.Recipe.findOne({ where: { id } });
+      // TODO: error dealing
+      recipe.destroy();
+      response.send({ success: true });
+    } catch (err) {
+      response.status(500).send(err);
+    }
   };
 
   // return all methods we define in an object
   // refer to the routes file above to see this used
   return {
     index,
+    show,
+    create,
+    update,
+    remove,
   };
 }
