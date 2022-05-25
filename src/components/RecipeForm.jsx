57eda9f571e9541aa0a655048d9e3caa1a01e7b7
diyanarmln <diyana.rmln@gmail.com>
2022-05-25 23:58:36 +0800

    added feature to prefill edit form and handle submit

--- src/components/RecipeForm.jsx
+++ src/components/RecipeForm.jsx
@@ -1,14 +1,34 @@
 import axios from 'axios';
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
 export default function RecipeForm() {
+  const params = useParams();
+  console.log('params', params.recipeId.length);
   const [title, setTitle] = useState('');
   const [servings, setServings] = useState();
   const [instructions, setInstructions] = useState('');
   const [category, setCategory] = useState();
   const [successMessage, setSuccessMessage] = useState('');
+  // let recipeData = {};
 
+  const getRecipe = () => {
+    try {
+      axios.get(`/api/recipe/${params.recipeId}`)
+        .then((response) => {
+          setTitle(response.data.title);
+          setServings(response.data.servings);
+          setInstructions(response.data.recipeInstructions);
+          setCategory(response.data.categories[0].id);
+          console.log(response.data); });
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  };
+
+  if (params.recipeId.length > 0) {
+    useEffect(() => { getRecipe(); }, []);
+  }
   // photo hallo
 
   const handleTitleChange = (e) => {
@@ -29,19 +49,32 @@ export default function RecipeForm() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      const { data } = await axios.post('/api/recipes', {
-        title, servings, instructions, category,
-      });
-      if (data.success) {
-        setSuccessMessage('Recipe saved success!');
-        setTitle('');
-        setServings('');
-        setInstructions('');
-        setCategory(0);
+    if (params.recipeId.length > 0) {
+      try {
+        const { data } = await axios.put(`/api/recipe/${params.recipeId}/edit`, {
+          title, servings, instructions, category,
+        });
+        if (data.success) {
+          setSuccessMessage('Recipe saved success!');
+        }
+      } catch (err) {
+        console.log(err.response.data);
       }
-    } catch (err) {
-      console.log(err.response.data);
+    } else {
+      try {
+        const { data } = await axios.post('/api/recipes', {
+          title, servings, instructions, category,
+        });
+        if (data.success) {
+          setSuccessMessage('Recipe saved success!');
+          setTitle('');
+          setServings('');
+          setInstructions('');
+          setCategory(0);
+        }
+      } catch (err) {
+        console.log(err.response.data);
+      }
     }
   };
 
@@ -71,7 +104,7 @@ export default function RecipeForm() {
         <div className="form-group">
           <label htmlFor="category">
             Category:
-            <select id="category" className="form-select" onChange={handleCategoryChange}>
+            <select id="category" className="form-select" onChange={handleCategoryChange} value={category}>
               <option value="1">Breakfast</option>
               <option value="2">Lunch</option>
               <option value="3">Dinner</option>
@@ -92,7 +125,7 @@ export default function RecipeForm() {
 
         <button type="submit" className="btn btn-primary" onClick={handleSubmit}>Save</button>
         <Link to="/recipes">
-          <button type="button" className="btn btn-outline-secondary">Cancel</button>
+          <button type="button" className="btn btn-outline-secondary">Back</button>
         </Link>
       </form>
       {successMessage}
