6a7dcc9f1cad8b02c60ca6000da9f2f3bdeabb0e
diyanarmln <diyana.rmln@gmail.com>
2022-05-26 01:42:57 +0800

    centered app component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,7 +4,14 @@ import { Link } from 'react-router-dom';
 export default function Dashboard() {
   return (
     <div className="App">
-      <h2>Welcome!</h2>
+      <h2>Hi Diyana</h2>
+      <p>Welcome back!</p>
+      <div className="card bg-dark text-white mb-3">
+        <img className="card-img" src="https://post.healthline.com/wp-content/uploads/2020/09/healthy-eating-ingredients-732x549-thumbnail-732x549.jpg" alt="recipe" />
+        <div className="card-img-overlay">
+          <h5 className="card-title">Recipe</h5>
+        </div>
+      </div>
       <div>
         <Link to="/recipes">
           <button type="button" className="btn btn-dark">Recipes</button>
