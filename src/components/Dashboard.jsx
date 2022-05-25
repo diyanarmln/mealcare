33c7cb79bc38eeee7185a569ec6d86774095bca8
diyanarmln <diyana.rmln@gmail.com>
2022-05-26 02:01:57 +0800

    adding image cards to dashboard to replace button navigation

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -6,21 +6,27 @@ export default function Dashboard() {
     <div className="App">
       <h2>Hi Diyana</h2>
       <p>Welcome back!</p>
-      <div className="card bg-dark text-white mb-3">
-        <img className="card-img" src="https://post.healthline.com/wp-content/uploads/2020/09/healthy-eating-ingredients-732x549-thumbnail-732x549.jpg" alt="recipe" />
-        <div className="card-img-overlay">
-          <h5 className="card-title">Recipe</h5>
+      <div className="arrange-card-nav">
+        <div className="center">
+          <Link to="/recipes">
+            <div className="card bg-dark text-white nav-card">
+              <img className="card-img" src="https://post.healthline.com/wp-content/uploads/2020/09/healthy-eating-ingredients-732x549-thumbnail-732x549.jpg" alt="recipe" />
+              <div className="card-img-overlay center-nav-text">
+                <h5 className="card-title header-nav">Recipes</h5>
+              </div>
+            </div>
+          </Link>
         </div>
-      </div>
-      <div>
-        <Link to="/recipes">
-          <button type="button" className="btn btn-dark">Recipes</button>
-        </Link>
-      </div>
-      <div>
-        <Link to="/planner">
-          <button type="button" className="btn btn-dark">Planner</button>
-        </Link>
+        <div className="center">
+          <Link to="/planner">
+            <div className="card bg-dark text-white nav-card">
+              <img className="card-img" src="https://media.istockphoto.com/photos/table-top-view-of-spicy-food-picture-id1316145932?b=1&k=20&m=1316145932&s=170667a&w=0&h=feyrNSTglzksHoEDSsnrG47UoY_XX4PtayUPpSMunQI=" alt="planner" />
+              <div className="card-img-overlay center-nav-text">
+                <h5 className="card-title header-nav">Planner</h5>
+              </div>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
