5f07a5a23844b9cca127c3464d99f4220cb5e091
diyanarmln <diyana.rmln@gmail.com>
2022-05-26 01:15:30 +0800

    added button for planner

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,9 +5,16 @@ export default function Dashboard() {
   return (
     <div className="App">
       <h2>Welcome!</h2>
-      <Link to="/recipes">
-        <button type="button" className="btn btn-dark">Recipes</button>
-      </Link>
+      <div>
+        <Link to="/recipes">
+          <button type="button" className="btn btn-dark">Recipes</button>
+        </Link>
+      </div>
+      <div>
+        <Link to="/planner">
+          <button type="button" className="btn btn-dark">Planner</button>
+        </Link>
+      </div>
     </div>
   );
 }
