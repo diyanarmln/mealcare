40b545a0090bec5132d74ae5d66ff20a1f1153e6
diyanarmln <diyana.rmln@gmail.com>
2022-05-26 01:23:19 +0800

    added placeholder code for planner component

---
+++ src/components/Planner.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+// import { Link } from 'react-router-dom';
+
+export default function Planner() {
+  return (
+    <div>
+      <h2>Your Meal Plan</h2>
+      <p>View and update your meal plan</p>
+    </div>
+  );
+}
