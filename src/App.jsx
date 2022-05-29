81de8332b3b8aa235d0de582ae06f7e5958127c2
diyanarmln <diyana.rmln@gmail.com>
2022-05-29 14:44:40 +0800

    added react route path to render planner component

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ export default function App() {
         <Route path="/new-recipe" element={<RecipeForm />} />
         <Route path="/recipe/:recipeId" element={<RecipeView />} />
         <Route path="/recipe/edit/:recipeId" element={<RecipeForm />} />
-        <Route path="/planner" element={<Planner />} />
+        <Route path="/planner/:plannerId" element={<Planner />} />
 
       </Routes>
     </BrowserRouter>
