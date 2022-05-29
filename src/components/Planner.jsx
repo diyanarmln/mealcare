77091d8ac5f495477134aae0971697ab33fa40ce
diyanarmln <diyana.rmln@gmail.com>
2022-05-29 13:48:32 +0800

    extract planner form to its own component

--- src/components/Planner.jsx
+++ src/components/Planner.jsx
@@ -1,38 +1,12 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 // import * as React from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Select from 'react-select';
+// import Select from 'react-select';
+import PlannerForm from './PlannerForm.jsx';
 
 export default function Planner() {
-  const [breakfastRecipes, setBreakfastRecipes] = useState([]);
-  const [lunchRecipes, setLunchRecipes] = useState([]);
-  const [dinnerRecipes, setDinnerRecipes] = useState([]);
-
-  const getRecipes = () => {
-    axios.get('/api/recipes')
-      .then((response) => {
-        const recipeList = response.data;
-        // eslint-disable-next-line no-return-assign
-        const breakfast = recipeList.filter((obj) => obj.categories[0].id === 1);
-        // eslint-disable-next-line max-len
-        const breakfastOptions = breakfast.map((recipe) => ({ label: recipe.title, value: recipe.title }));
-        setBreakfastRecipes(breakfastOptions);
-        // // eslint-disable-next-line no-return-assign
-        const lunch = recipeList.filter((obj) => obj.categories[0].id === 2);
-        const lunchOptions = lunch.map((recipe) => ({ label: recipe.title, value: recipe.title }));
-        setLunchRecipes(lunchOptions);
-        // // eslint-disable-next-line no-return-assign
-        const dinner = recipeList.filter((obj) => obj.categories[0].id === 3);
-        // eslint-disable-next-line max-len
-        const dinnerOptions = dinner.map((recipe) => ({ label: recipe.title, value: recipe.title }));
-        setDinnerRecipes(dinnerOptions);
-      });
-  };
-
-  useEffect(() => { getRecipes(); }, []);
-
   return (
     <div className="App">
       <div className="page-title">
@@ -55,14 +29,7 @@ export default function Planner() {
         <div className="day-btn">S</div>
       </div>
 
-      <h4>Breakfast</h4>
-      <Select options={breakfastRecipes} />
-      <br />
-      <h4>Lunch</h4>
-      <Select options={lunchRecipes} />
-      <br />
-      <h4>Dinner</h4>
-      <Select options={dinnerRecipes} />
+      <PlannerForm />
 
     </div>
   );
