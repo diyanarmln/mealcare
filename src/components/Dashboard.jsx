05074447677c9c45bdb177cd00a2e64eebae515b
diyanarmln <diyana.rmln@gmail.com>
2022-05-29 14:44:58 +0800

    updated react planner route/path

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -18,7 +18,7 @@ export default function Dashboard() {
           </Link>
         </div>
         <div className="center">
-          <Link to="/planner">
+          <Link to="/planner/1">
             <div className="card bg-dark text-white nav-card">
               <img className="card-img" src="https://media.istockphoto.com/photos/table-top-view-of-spicy-food-picture-id1316145932?b=1&k=20&m=1316145932&s=170667a&w=0&h=feyrNSTglzksHoEDSsnrG47UoY_XX4PtayUPpSMunQI=" alt="planner" />
               <div className="card-img-overlay center-nav-text">
