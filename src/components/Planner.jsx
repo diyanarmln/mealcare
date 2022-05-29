90363f8ff378bf608a8335a2bb162102ef87aaab
diyanarmln <diyana.rmln@gmail.com>
2022-05-29 12:29:42 +0800

    added days of the week in planner with styling

--- src/components/Planner.jsx
+++ src/components/Planner.jsx
@@ -1,16 +1,30 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Planner() {
   return (
     <div className="App">
-      <h2>
-        <Link to="/" className="recipe-title">
-          <i className="fa fa-chevron-left" style={{ 'margin-right': '10px' }} aria-hidden="true" />
-        </Link>
-        Your Meal Plan
-      </h2>
-      <p>View and update your meal plan</p>
+      <div className="page-title">
+        <h2>
+          <Link to="/" className="recipe-title">
+            <i className="fa fa-chevron-left" style={{ 'margin-right': '10px' }} aria-hidden="true" />
+          </Link>
+          Your Meal Plan
+        </h2>
+        <p>View and update your meal plan</p>
+      </div>
+
+      <div className="week-layout">
+        <div className="day-btn">M</div>
+        <div className="day-btn">T</div>
+        <div className="day-btn">W</div>
+        <div className="day-btn">T</div>
+        <div className="day-btn">F</div>
+        <div className="day-btn">S</div>
+        <div className="day-btn">S</div>
+      </div>
+
     </div>
   );
 }
