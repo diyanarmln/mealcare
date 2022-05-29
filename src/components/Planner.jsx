f574d32186198de0cc309ad030540262e8acad68
diyanarmln <diyana.rmln@gmail.com>
2022-05-29 11:52:49 +0800

    add back button

--- src/components/Planner.jsx
+++ src/components/Planner.jsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Planner() {
   return (
     <div className="App">
-      <h2>Your Meal Plan</h2>
+      <h2>
+        <Link to="/" className="recipe-title">
+          <i className="fa fa-chevron-left" style={{ 'margin-right': '10px' }} aria-hidden="true" />
+        </Link>
+        Your Meal Plan
+      </h2>
       <p>View and update your meal plan</p>
     </div>
   );
