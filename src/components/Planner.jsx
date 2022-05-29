3318d642829cc1e6c9acd3347c61578ddfce8c75
diyanarmln <diyana.rmln@gmail.com>
2022-05-29 13:29:22 +0800

    added components for recipe selection

--- src/components/Planner.jsx
+++ src/components/Planner.jsx
@@ -1,8 +1,38 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import * as React from 'react';
+// import * as React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Select from 'react-select';
 
 export default function Planner() {
+  const [breakfastRecipes, setBreakfastRecipes] = useState([]);
+  const [lunchRecipes, setLunchRecipes] = useState([]);
+  const [dinnerRecipes, setDinnerRecipes] = useState([]);
+
+  const getRecipes = () => {
+    axios.get('/api/recipes')
+      .then((response) => {
+        const recipeList = response.data;
+        // eslint-disable-next-line no-return-assign
+        const breakfast = recipeList.filter((obj) => obj.categories[0].id === 1);
+        // eslint-disable-next-line max-len
+        const breakfastOptions = breakfast.map((recipe) => ({ label: recipe.title, value: recipe.title }));
+        setBreakfastRecipes(breakfastOptions);
+        // // eslint-disable-next-line no-return-assign
+        const lunch = recipeList.filter((obj) => obj.categories[0].id === 2);
+        const lunchOptions = lunch.map((recipe) => ({ label: recipe.title, value: recipe.title }));
+        setLunchRecipes(lunchOptions);
+        // // eslint-disable-next-line no-return-assign
+        const dinner = recipeList.filter((obj) => obj.categories[0].id === 3);
+        // eslint-disable-next-line max-len
+        const dinnerOptions = dinner.map((recipe) => ({ label: recipe.title, value: recipe.title }));
+        setDinnerRecipes(dinnerOptions);
+      });
+  };
+
+  useEffect(() => { getRecipes(); }, []);
+
   return (
     <div className="App">
       <div className="page-title">
@@ -24,6 +54,15 @@ export default function Planner() {
         <div className="day-btn">S</div>
         <div className="day-btn">S</div>
       </div>
+
+      <h3>Breakfast</h3>
+      <Select options={breakfastRecipes} />
+      <br />
+      <h3>Lunch</h3>
+      <Select options={lunchRecipes} />
+      <br />
+      <h3>Dinner</h3>
+      <Select options={dinnerRecipes} />
 
     </div>
   );
