2cf663efb3898646b1e29ab800475f6ff5d35995
diyanarmln <diyana.rmln@gmail.com>
2022-05-30 20:24:45 +0800

    added controller to save meal plans

--- controllers/plans.mjs
+++ controllers/plans.mjs
@@ -18,7 +18,28 @@ export default function initPlansController(db) {
       })
       .catch((error) => console.log(error));
   };
+
+  const update = async (request, response) => {
+    const { plannerId } = request.params;
+    const plan = await db.Plan.findOne({ where: { id: plannerId } });
+    const updatedPlan = {
+      breakfastRecipe: request.body.breakfastMealInput,
+      lunchRecipe: request.body.lunchMealInput,
+      dinnerRecipe: request.body.dinnerMealInput,
+      updatedAt: new Date(),
+    };
+
+    try {
+      await plan.update(updatedPlan);
+      response.send({ success: true });
+    } catch (error) {
+      response.status(500).send(error);
+      console.log(error);
+    }
+  };
+
   return {
     show,
+    update,
   };
 }
