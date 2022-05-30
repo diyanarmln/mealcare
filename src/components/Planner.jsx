95fd3b6449caf63c9b91fb9bd09534e4e93cff56
diyanarmln <diyana.rmln@gmail.com>
2022-05-31 01:53:26 +0800

    updated links and week day buttons

--- src/components/Planner.jsx
+++ src/components/Planner.jsx
@@ -1,10 +1,8 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 // import * as React from 'react';
 // import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-// import Select from 'react-select';
-import PlannerForm from './PlannerForm.jsx';
 
 export default function Planner() {
   return (
@@ -20,37 +18,35 @@ export default function Planner() {
       </div>
 
       <div className="week-layout">
-        <Link to="/planner/1" className="">
-          <div id="1" className="day-btn">M</div>
+        <Link to={{ pathname: '/planner/1' }} className="">
+          <button type="button" id="1" className="day-btn">M</button>
         </Link>
 
-        <Link to="/planner/2" className="">
-          <div id="2" className="day-btn">T</div>
+        <Link to={{ pathname: '/planner/2' }} className="">
+          <button type="button" id="2" className="day-btn">T</button>
         </Link>
 
-        <Link to="/planner/3" className="">
-          <div id="3" className="day-btn">W</div>
+        <Link to={{ pathname: '/planner/3' }} className="">
+          <button type="button" id="3" className="day-btn">W</button>
         </Link>
 
-        <Link to="/planner/4" className="">
-          <div id="4" className="day-btn">T</div>
+        <Link to={{ pathname: '/planner/4' }} className="">
+          <button type="button" id="4" className="day-btn">T</button>
         </Link>
 
-        <Link to="/planner/5" className="">
-          <div id="5" className="day-btn">F</div>
+        <Link to={{ pathname: '/planner/5' }} className="">
+          <button type="button" id="5" className="day-btn">F</button>
         </Link>
 
-        <Link to="/planner/6" className="">
-          <div id="6" className="day-btn">S</div>
+        <Link to={{ pathname: '/planner/6' }} className="">
+          <button type="button" id="6" className="day-btn">S</button>
         </Link>
 
-        <Link to="/planner/7" className="">
-          <div id="7" className="day-btn">S</div>
+        <Link to={{ pathname: '/planner/7' }} className="">
+          <button type="button" id="7" className="day-btn">S</button>
         </Link>
 
       </div>
-
-      <PlannerForm />
 
     </div>
   );
