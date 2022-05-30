067ea496f0a882d6866e67733f3e650abd284fb7
diyanarmln <diyana.rmln@gmail.com>
2022-05-30 20:25:03 +0800

    added onchange function to save selected meals

--- src/components/PlannerForm.jsx
+++ src/components/PlannerForm.jsx
@@ -10,7 +10,7 @@ export default function PlannerForm() {
   const [lunchRecipes, setLunchRecipes] = useState([]);
   const [dinnerRecipes, setDinnerRecipes] = useState([]);
 
-  const [hasPlan, setHasPlan] = useState(false);
+  // const [hasPlan, setHasPlan] = useState(false);
 
   const [breakfastMeal, setBreakfastMeal] = useState('');
   const [lunchMeal, setLunchMeal] = useState('');
@@ -18,18 +18,18 @@ export default function PlannerForm() {
 
   console.log(params.plannerId);
 
-  const getPlan = () => {
-    axios.get(`/api/plan/${params.plannerId}`)
-      .then((response) => {
-        console.log(response.data.length);
-        if (response.data.length !== undefined) {
-          setHasPlan(true);
-        }
-      });
-  };
+  // const getPlan = () => {
+  //   axios.get(`/api/plan/${params.plannerId}`)
+  //     .then((response) => {
+  //       console.log(response.data.length);
+  //       if (response.data.length !== undefined) {
+  //         setHasPlan(true);
+  //       }
+  //     });
+  // };
 
-  useEffect(() => { getPlan(); }, []);
-  console.log(hasPlan);
+  // useEffect(() => { getPlan(); }, []);
+  // console.log(hasPlan);
 
   const getRecipes = () => {
     axios.get('/api/recipes')
@@ -38,24 +38,63 @@ export default function PlannerForm() {
         // eslint-disable-next-line no-return-assign
         const breakfast = recipeList.filter((obj) => obj.categories[0].id === 1);
         // eslint-disable-next-line max-len
-        const breakfastOptions = breakfast.map((recipe) => ({ label: recipe.title, value: recipe.title }));
+        const breakfastOptions = breakfast.map((recipe) => ({ label: recipe.title, value: recipe.id }));
         setBreakfastRecipes(breakfastOptions);
         // // eslint-disable-next-line no-return-assign
         const lunch = recipeList.filter((obj) => obj.categories[0].id === 2);
-        const lunchOptions = lunch.map((recipe) => ({ label: recipe.title, value: recipe.title }));
+        const lunchOptions = lunch.map((recipe) => ({ label: recipe.title, value: recipe.id }));
         setLunchRecipes(lunchOptions);
         // // eslint-disable-next-line no-return-assign
         const dinner = recipeList.filter((obj) => obj.categories[0].id === 3);
         // eslint-disable-next-line max-len
-        const dinnerOptions = dinner.map((recipe) => ({ label: recipe.title, value: recipe.title }));
+        const dinnerOptions = dinner.map((recipe) => ({ label: recipe.title, value: recipe.id }));
         setDinnerRecipes(dinnerOptions);
       });
   };
-
   useEffect(() => { getRecipes(); }, []);
+  console.log(breakfastRecipes);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+  };
+
+  const handleBreakfastOnChange = async (e) => {
+    try {
+      setBreakfastMeal(e.value);
+      const breakfastMealInput = e.value;
+      const { data } = await axios.put(`/api/plan/${params.plannerId}`, { breakfastMealInput });
+      if (data.success) {
+        console.log('Breakfast meal plan saved successfully');
+      }
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  };
+
+  const handleLunchOnChange = async (e) => {
+    try {
+      setLunchMeal(e.value);
+      const lunchMealInput = e.value;
+      const { data } = await axios.put(`/api/plan/${params.plannerId}`, { lunchMealInput });
+      if (data.success) {
+        console.log('Lunch meal plan saved successfully');
+      }
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  };
+
+  const handleDinnerOnChange = async (e) => {
+    try {
+      setDinnerMeal(e.value);
+      const dinnerMealInput = e.value;
+      const { data } = await axios.put(`/api/plan/${params.plannerId}`, { dinnerMealInput });
+      if (data.success) {
+        console.log('Dinner meal plan saved successfully');
+      }
+    } catch (err) {
+      console.log(err.response.data);
+    }
   };
 
   return (
@@ -63,25 +102,19 @@ export default function PlannerForm() {
       <h4>Breakfast</h4>
       <Select
         options={breakfastRecipes}
-        onChange={(e) => {
-          setBreakfastMeal(e.value);
-        }}
+        onChange={handleBreakfastOnChange}
       />
       <br />
       <h4>Lunch</h4>
       <Select
         options={lunchRecipes}
-        onChange={(e) => {
-          setLunchMeal(e.value);
-        }}
+        onChange={handleLunchOnChange}
       />
       <br />
       <h4>Dinner</h4>
       <Select
         options={dinnerRecipes}
-        onChange={(e) => {
-          setDinnerMeal(e.value);
-        }}
+        onChange={handleDinnerOnChange}
       />
       <br />
       <div>
