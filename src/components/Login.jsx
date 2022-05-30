ee80f61e7f2af0a4cd124630391e93697cc7631e
diyanarmln <diyana.rmln@gmail.com>
2022-05-31 07:17:27 +0800

    created components for login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,39 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
+  const navigate = useNavigate();
+  const [emailInput, setEmailInput] = useState('');
+  const [passwordInput, setPasswordInput] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('/api/login', { emailInput, passwordInput })
+        .then((response) => {
+          console.log(response.data);
+          axios
+            .get('/api/user')
+            .then((dashboardResponse) => {
+              console.log(dashboardResponse);
+              navigate('/home');
+            });
+        });
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  };
+
+  const handleEmailChange = (e) => {
+    setEmailInput(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPasswordInput(e.target.value);
+  };
+
   return (
     <div className="App">
       <section className="vh-100">
@@ -14,27 +46,26 @@ export default function Login() {
                   <div className="mb-md-5 mt-md-4 pb-5">
 
                     <h2 className="fw-bold mb-2 login-text">MealCare</h2>
-                    {/* <p className="text-white-50 mb-5 login-text">Please enter your login and password!</p> */}
 
                     <div className="form-outline form-white mb-4">
 
                       <label className="form-label" htmlFor="typeEmailX">
                         Email
-                        <input type="email" id="typeEmailX" className="form-control form-control-lg login-labels" />
+                        <input type="email" id="typeEmailX" className="form-control form-control-lg login-labels" value={emailInput} onChange={handleEmailChange} />
                       </label>
                     </div>
 
                     <div className="form-outline form-white mb-4">
                       <label className="form-label" htmlFor="typePasswordX">
                         Password
-                        <input type="password" id="typePasswordX" className="form-control form-control-lg login-labels" />
+                        <input type="password" id="typePasswordX" className="form-control form-control-lg login-labels" value={passwordInput} onChange={handlePasswordChange} />
 
                       </label>
                     </div>
 
                     <p className="small mb-5 pb-lg-2"><a className="text-white-50" href="#!">Forgot password?</a></p>
 
-                    <button className="btn btn-outline-light btn-lg px-5 login-btn" type="submit">Login</button>
+                    <button className="btn btn-outline-light btn-lg px-5 login-btn" type="submit" onClick={handleSubmit}>Login</button>
 
                     <div className="d-flex justify-content-center text-center mt-4 pt-1">
                       <a href="#!" className="text-white"><i className="fa fa-facebook-f fa-lg" /></a>
